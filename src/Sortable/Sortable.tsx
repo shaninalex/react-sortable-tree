3fa3f71f387d96aeb5f01dff60d829c358338e43
shaninalex <shanin.x@gmail.com>
2024-10-22 17:32:24 +0300

    move nested elements but not sorting

--- src/Sortable/Sortable.tsx
+++ src/Sortable/Sortable.tsx
@@ -15,11 +15,23 @@ interface SortableItemProps {
 }
 
 export function SortableItem(props: SortableItemProps) {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: props.id })
+    const { group, id } = props
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id })
+
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates
+        })
+    )
 
     const style = {
         transform: CSS.Transform.toString(transform),
         transition,
+    }
+
+    function handleDragStart(event: DragStartEvent) {
+        console.log(event)
     }
 
     return (
@@ -28,7 +40,11 @@ export function SortableItem(props: SortableItemProps) {
                 {props.id.split('-')[0]}
             </div>
             <div className='flex flex-col gap-2'>
-                {props.group.groups.map(group => <SortableItem key={group.id} id={group.id} group={group} />)}
+                <DndContext sensors={sensors} collisionDetection={closestCorners} onDragStart={handleDragStart}>
+                    <SortableContext items={group.groups} strategy={verticalListSortingStrategy}>
+                        {group.groups.map(g => <SortableItem key={g.id} id={g.id} group={g} />)}
+                    </SortableContext>
+                </DndContext>
             </div>
         </div>
     )
@@ -48,6 +64,7 @@ export const Sortable = () => {
     function handleDragStart(event: DragStartEvent) {
         const { active } = event
         const { id } = active
+        console.log(event)
         setActiveId(id)
     }
 
